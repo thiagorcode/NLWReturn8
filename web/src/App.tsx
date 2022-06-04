76771b8be9b7b2505414e637325e3a8806e77ec0
Thiago Rodrigues <ti.thiago.rodrigues@gmail.com>
2022-06-04 00:26:25 -0300

    Part

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-
-import { Widget } from './components/Widget'
+import { Widget } from './components/Widget';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <Widget />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,9 +1,9 @@
 import { ChatTeardropDots } from 'phosphor-react';
-import { Popover } from '@headlessui/react'
+import { Popover } from '@headlessui/react';
 
 export function Widget() {
   return (
-    <Popover className="absolute bottom-5 right-5">
+    <Popover className="absolute bottom-5 right-5 flex flex-col items-end">
       <Popover.Panel>Hello</Popover.Panel>
       <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
@@ -13,5 +13,5 @@ export function Widget() {
         </span>
       </Popover.Button>
     </Popover>
-  )
+  );
 }
